901b82c232dd46c7f092cf9bab0bc8fade54802b
Flavio-LP <flavio5070@outlook.com>
2025-09-24 11:17:47 -0300

    feat: create pull conection pgsql and test query route from db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask
+from .database import db
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    
 
     from .routes import bp as routes_bp
     app.register_blueprint(routes_bp)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
+from app.database import db
+
+load_dotenv()
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
