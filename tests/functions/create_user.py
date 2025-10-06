f59c09470ba5b3fd4ed9bf0b43beddd1ed0b7095
Flavio-LP <flavio5070@outlook.com>
2025-10-06 13:02:38 -0300

    test: inserted a new tests

--- tests/functions/create_user.py
+++ tests/functions/create_user.py
@@ -3,7 +3,14 @@ import requests
 def create_user():
 
     return requests.post('http://127.0.0.1:5000/register',json = {
-                "name" : "joana",
-                "email" : "joana@joana.com",
+                "name" : "pablo",
+                "email" : "pablo@.pablocom",
+                "password" : "1234"
+        })
+
+def create_user_erros():
+
+    return requests.post('http://127.0.0.1:5000/register',json = {
+                "name" : "pablo",
                 "password" : "1234"
         })
--- tests/test_create_user.py
+++
@@ -1,9 +0,0 @@
-from functions.create_user import create_user
-
-def test_create_user():
-    req = create_user()
-    assert req.status_code == 201
-
-def test_user_exists():
-    req = create_user()
-    assert req.status_code == 409
