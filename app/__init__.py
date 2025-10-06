c7cffdd685aca0a5237a09d11747fb97af1f7416
Flavio-LP <flavio5070@outlook.com>
2025-10-06 17:12:36 -0300

    docs: Swagger created, fix a bug in return error when expired token

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flasgger import Swagger
 from .database import db
 from dotenv import load_dotenv
 from . import models
@@ -8,6 +9,23 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": 'apispec',
+                "route": '/apispec.json',
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/"
+    }
+    
+    swagger = Swagger(app, config=swagger_config)
 
     db.init_app(app)
 
